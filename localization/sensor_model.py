ae1062d0d7e66627c3452d192350e58644ffd072
JohlesaO <johlesa@icloud.com>
2024-04-03 16:07:41 -0400

    went back to old code

--- localization/sensor_model.py
+++ localization/sensor_model.py
@@ -1,3 +1,126 @@
+# import numpy as np
+# from scan_simulator_2d import PyScanSimulator2D
+# # Try to change to just `from scan_simulator_2d import PyScanSimulator2D` 
+# # if any error re: scan_simulator_2d occurs
+
+# from tf_transformations import euler_from_quaternion
+
+# from nav_msgs.msg import OccupancyGrid
+
+# import sys
+
+# np.set_printoptions(threshold=sys.maxsize)
+# import math
+
+# class SensorModel:
+
+#     def __init__(self, node):
+#         node.declare_parameter('map_topic', "default")
+#         node.declare_parameter('num_beams_per_particle', "default")
+#         node.declare_parameter('scan_theta_discretization', "default")
+#         node.declare_parameter('scan_field_of_view', "default")
+#         node.declare_parameter('lidar_scale_to_map_scale', 1)
+
+#         self.map_topic = node.get_parameter('map_topic').get_parameter_value().string_value
+#         self.num_beams_per_particle = node.get_parameter('num_beams_per_particle').get_parameter_value().integer_value
+#         self.scan_theta_discretization = node.get_parameter(
+#             'scan_theta_discretization').get_parameter_value().double_value
+#         self.scan_field_of_view = node.get_parameter('scan_field_of_view').get_parameter_value().double_value
+#         self.lidar_scale_to_map_scale = node.get_parameter(
+#             'lidar_scale_to_map_scale').get_parameter_value().double_value
+
+#         ####################################
+#         # Adjust these parameters
+#         self.alpha_hit = 0
+#         self.alpha_short = 0
+#         self.alpha_max = 0
+#         self.alpha_rand = 0
+#         self.sigma_hit = 0
+#         self.epsilon = 0
+
+#         # Your sensor table will be a `table_width` x `table_width` np array:
+#         self.table_width = 201
+#         ####################################
+
+#         node.get_logger().info("%s" % self.map_topic)
+#         node.get_logger().info("%s" % self.num_beams_per_particle)
+#         node.get_logger().info("%s" % self.scan_theta_discretization)
+#         node.get_logger().info("%s" % self.scan_field_of_view)
+
+#         # Precompute the sensor model table
+#         self.sensor_model_table = np.empty((self.table_width, self.table_width))
+#         self.precompute_sensor_model()
+
+#         # Create a simulated laser scan
+#         self.scan_sim = PyScanSimulator2D(
+#             self.num_beams_per_particle,
+#             self.scan_field_of_view,
+#             0,  # This is not the simulator, don't add noise
+#             0.01,  # This is used as an epsilon
+#             self.scan_theta_discretization)
+
+#         # Subscribe to the map
+#         self.map = None
+#         self.map_set = False
+#         self.map_subscriber = node.create_subscription(
+#             OccupancyGrid,
+#             self.map_topic,
+#             self.map_callback,
+#             1)
+
+#     def precompute_sensor_model(self):
+#         """
+#         Generate and store a table which represents the sensor model.
+
+#         For each discrete computed range value, this provides the probability of 
+#         measuring any (discrete) range. This table is indexed by the sensor model
+#         at runtime by discretizing the measurements and computed ranges from
+#         RangeLibc.
+#         This table must be implemented as a numpy 2D array.
+
+#         Compute the table based on class parameters alpha_hit, alpha_short,
+#         alpha_max, alpha_rand, sigma_hit, and table_width.
+
+#         args:
+#             N/A
+
+#         returns:
+#             No return type. Directly modify `self.sensor_model_table`.
+#         """
+
+#         e = math.e
+#         pi = math.pi
+#         table = []
+        
+#         phit_array = np.empty((self.table_width, self.table_width))
+#         pshort_array = np.empty((self.table_width, self.table_width))
+#         pmax_array = np.empty((self.table_width, self.table_width))
+#         prand_array = np.empty((self.table_width, self.table_width))
+#         zmax =200
+
+#         for zk_i in range(self.table_width):
+#             for d in range(self.table_width):
+#                 phit = (1/math.sqrt(2*pi*self.sigma_hit**2))*e**(-((zk_i - d)**2)/(2*self.sigma_hit**2)) if zk_i <= zmax else 0 
+#                 pshort = (2/d)*(1 - zk_i/d) if zk_i<=d else 0 
+#                 pmax = 1/self.epsilon if zk_i <= zmax and zmax - self.epsilon <= zk_i else 0 
+#                 prand = 1/zmax if zk_i <= zmax else 0 
+
+#                 phit_array[zk_i][d] = phit 
+#                 pshort_array[zk_i][d] = pshort
+#                 pmax_array[zk_i][d] = pmax
+#                 prand_array[zk_i][d] = prand
+
+#         for zk_i in range(self.table_width):
+#             for d in range(self.table_width):
+#                 p_zki = self.alpha_hit*phit + self.alpha_short*pshort + self.alpha_max*pmax + self.alpha_rand*prand
+#                 table[zk_i][d] = p_zki
+
+#         for col in range(self.table_width):
+#             table[:,col] = np.normalize(table[:,col])
+
+#         self.sensor_model_table = table
+
+
 import numpy as np
 from scan_simulator_2d import PyScanSimulator2D
 # Try to change to just `from scan_simulator_2d import PyScanSimulator2D` 
@@ -10,7 +133,7 @@ from nav_msgs.msg import OccupancyGrid
 import sys
 
 np.set_printoptions(threshold=sys.maxsize)
-import math
+
 
 class SensorModel:
 
@@ -31,12 +154,14 @@ class SensorModel:
 
         ####################################
         # Adjust these parameters
-        self.alpha_hit = 0
-        self.alpha_short = 0
-        self.alpha_max = 0
-        self.alpha_rand = 0
-        self.sigma_hit = 0
-        self.epsilon = 0
+        self.alpha_hit = 0.74
+        self.alpha_short = 0.07
+        self.alpha_max = 0.07
+        self.alpha_rand = 0.12
+        self.sigma_hit = 0.5
+        self.z_max = 1
+        self.n = 1
+        self.normalization_constant = 1
 
         # Your sensor table will be a `table_width` x `table_width` np array:
         self.table_width = 201
@@ -49,6 +174,7 @@ class SensorModel:
 
         # Precompute the sensor model table
         self.sensor_model_table = np.empty((self.table_width, self.table_width))
+        self.p_hit_table = np.empty((self.table_width, self.table_width))
         self.precompute_sensor_model()
 
         # Create a simulated laser scan
@@ -87,39 +213,32 @@ class SensorModel:
         returns:
             No return type. Directly modify `self.sensor_model_table`.
         """
-
-        e = math.e
-        pi = math.pi
-        table = []
         
-        phit_array = np.empty((self.table_width, self.table_width))
-        pshort_array = np.empty((self.table_width, self.table_width))
-        pmax_array = np.empty((self.table_width, self.table_width))
-        prand_array = np.empty((self.table_width, self.table_width))
-        zmax =200
+        for z_i in range(self.table_width): # z_i is the rows
+            for d_i in range(self.table_width): # d is the columns
+                self.p_hit_table[z_i][d_i] = self.p_hit(z_i, d_i)
+        self.p_hit_table = np.linalg.norm(self.p_hit_table, axis=1, norm='max') # normalize the columns of the hit table
 
-        for zk_i in range(self.table_width):
-            for d in range(self.table_width):
-                phit = (1/math.sqrt(2*pi*self.sigma_hit**2))*e**(-((zk_i - d)**2)/(2*self.sigma_hit**2)) if zk_i <= zmax else 0 
-                pshort = (2/d)*(1 - zk_i/d) if zk_i<=d else 0 
-                pmax = 1/self.epsilon if zk_i <= zmax and zmax - self.epsilon <= zk_i else 0 
-                prand = 1/zmax if zk_i <= zmax else 0 
+        for z_i in range(self.table_width):
+            for d_i in range(self.table_width):
+                self.sensor_model_table[z_i][d_i] = self.add_probablities(z_i, d_i, self.p_hit_table[z_i][d_i])
 
-                phit_array[zk_i][d] = phit 
-                pshort_array[zk_i][d] = pshort
-                pmax_array[zk_i][d] = pmax
-                prand_array[zk_i][d] = prand
+        self.sensor_model_table = np.linalg.norm(self.sensor_model_table, axis=1, norm='max') # normalize the columns
 
-        for zk_i in range(self.table_width):
-            for d in range(self.table_width):
-                p_zki = self.alpha_hit*phit + self.alpha_short*pshort + self.alpha_max*pmax + self.alpha_rand*prand
-                table[zk_i][d] = p_zki
+    def add_probablities(self, z, d, p_hit_val):
+        return self.alpha_hit*p_hit_val+self.alpha_short*self.p_short(z, d)+self.alpha_max*self.p_max(z)+self.alpha_rand*self.p_rand(z)
 
-        for col in range(self.table_width):
-            table[:,col] = np.normalize(table[:,col])
+    def p_hit(self, z, d):
+        return self.n*1/np.sqrt(2*np.pi*self.sigma_hit**2)*np.exp(-(z-d)**2/(2*self.sigma_hit**2)) if 0 <= z <= self.z_max else 0
 
-        self.sensor_model_table = table
-        
+    def p_short(self, z, d):
+        return 2/d*(1-z/d) if (0<=z<=d and d!=0) else 0
+
+    def p_max(self, z):
+        return 1/self.epsilon if self.z_max-self.epsilon <= z <= self.z_max else 0
+
+    def p_rand(self, z):
+        return 1/self.z_max if 0 <= z <= self.z_max else 0        
 
     def evaluate(self, particles, observation):
         """
