3efc53391a80763b4568839d753221aaf2ab1fe4
VasuKaker <vasuk@mit.edu>
2024-04-07 18:36:00 -0400

    changes

--- localization/particle_filter.py
+++ localization/particle_filter.py
@@ -124,7 +124,7 @@ class ParticleFilter(Node):
         x = msg.pose.pose.position.x
         y = msg.pose.pose.position.y
         # euler fr/ q
-        odom_quat = tf.euler_from_quaternion(0, 0, msg.pose.pose.orientation)
+        odom_quat = tf.euler_from_quaternion(msg.pose.pose.orientation)
         th = odom_quat[3]
 
         od = [x, y, th]
