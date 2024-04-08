ef0be55234a5e58c303c21ce22457e2cdd642ada
JohlesaO <johlesa@icloud.com>
2024-04-08 17:23:48 -0400

    might work??

--- localization/particle_filter.py
+++ localization/particle_filter.py
@@ -157,11 +157,11 @@ class ParticleFilter(Node):
         newx = x + np.random.normal(loc=0.0, scale=0.1, size=(len(self.particles),1))
         newy = y + np.random.normal(loc=0.0, scale=0.1, size=(len(self.particles),1))
         # newth = np.angle(np.exp(1j * (th + np.random.default_rng().uniform(low=0.0, high=2*np.pi, size=len(self.particles)))))
-
+        newth = th + np.random.normal(loc=0.0, scale=0.01, size=(len(self.particles),1))
         # self.get_logger().info(np.array_str(newx))
         # self.get_logger().info(np.array_str(newy))
         # self.get_logger().info(np.array_str(newth))
-        self.particles = np.concatenate((newx, newy, np.full(shape=(len(self.particles),1), fill_value=th)), axis=1)
+        self.particles = np.concatenate((newx, newy, newth, np.full(shape=(len(self.particles),1), fill_value=th)), axis=1)
 
         # self.get_logger().info("*******Initialized particles from pose******")
         self.publish_transform(self.particles)
@@ -176,13 +176,6 @@ class ParticleFilter(Node):
 
         avg_x = np.average(particles[:,0])
         avg_y = np.average(particles[:,1])
-        
-        # particle_pose = [avg_x, avg_y, avg_angle]
-        
-        #publish transform between /map frame and frame for exp car base link
-        # tf_map_baselink = self.sensor_model.map @ particle_pose
-        # now = self.get_clock().now()
-        # out = self.se3_to_tf(tf_map_baselink, now, parent='map', child='base_link')
         
         odom_pub_msg = Odometry() # creating message template in case ros is mad
         odom_pub_msg.pose.pose.position.x = avg_x #out # specifically ONLY publish to the pose variable
@@ -211,49 +204,6 @@ class ParticleFilter(Node):
             poses.append(pose_msg)
         particles_msg.poses = poses
         self.particles_pub.publish(particles_msg)
-
-
-    # def tf_to_se3(self, transform: TransformStamped.transform) -> np.ndarray:
-    #     """
-    #     Convert a TransformStamped message to a 4x4 SE3 matrix 
-    #     """
-    #     q = transform.rotation
-    #     q = [q.x, q.y, q.z, q.w]
-    #     t = transform.translation
-    #     mat = tf.quaternion_matrix(q)
-    #     mat[0, 3] = t.x
-    #     mat[1, 3] = t.y
-    #     mat[2, 3] = t.z
-    #     return mat
-
-    # def se3_to_tf(self, mat: np.ndarray, time: Time, parent: str, child: str) -> TransformStamped:
-    #     """
-    #     Convert a 4x4 SE3 matrix to a TransformStamped message
-    #     """
-    #     obj = geometry_msgs.msg.TransformStamped()
-
-    #     # current time
-    #     obj.header.stamp = time.to_msg()
-
-    #     # frame names
-    #     obj.header.frame_id = parent
-    #     obj.child_frame_id = child
-
-    #     # translation component
-    #     obj.transform.translation.x = mat[0, 3]
-    #     obj.transform.translation.y = mat[1, 3]
-    #     obj.transform.translation.z = mat[2, 3]
-
-    #     # rotation (quaternion)
-    #     q = tf.quaternion_from_matrix(mat)
-    #     obj.transform.rotation.x = q[0]
-    #     obj.transform.rotation.y = q[1]
-    #     obj.transform.rotation.z = q[2]
-    #     obj.transform.rotation.w = q[3]
-
-    #     return obj
-
-
 
 def main(args=None):
     rclpy.init(args=args)
