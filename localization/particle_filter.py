34964fb558e5a03ee182b0eb20e4785ba589db22
JohlesaO <johlesa@icloud.com>
2024-04-09 01:40:54 -0400

    working good enough

--- localization/particle_filter.py
+++ localization/particle_filter.py
@@ -4,8 +4,8 @@ from rclpy.time import Time
 
 from nav_msgs.msg import Odometry
 import geometry_msgs
-from geometry_msgs.msg import PoseWithCovarianceStamped, TransformStamped, PoseArray
-import tf_transformations
+from geometry_msgs.msg import PoseWithCovarianceStamped, TransformStamped, PoseArray, Pose, Quaternion
+import tf_transformations as tf
 
 from rclpy.node import Node
 import rclpy
@@ -13,6 +13,7 @@ from sensor_msgs.msg import LaserScan
 
 assert rclpy
 import numpy as np
+import threading
 
 
 class ParticleFilter(Node):
@@ -22,7 +23,8 @@ class ParticleFilter(Node):
 
         self.declare_parameter('particle_filter_frame', "default")
         self.particle_filter_frame = self.get_parameter('particle_filter_frame').get_parameter_value().string_value
-        self.particles = np.zeros((100,3))
+        self.particles = np.zeros((200,3))
+        self.mutex = threading.Lock()
 
         #  *Important Note #1:* It is critical for your particle
         #     filter to obtain the following topic names from the
@@ -72,8 +74,6 @@ class ParticleFilter(Node):
         self.motion_model = MotionModel(self)
         self.sensor_model = SensorModel(self)
 
-        self.get_logger().info("=============+READY+=============")
-
         # Implement the MCL algorithm
         # using the sensor model and the motion model
         #
@@ -85,54 +85,43 @@ class ParticleFilter(Node):
         # and the particle_filter_frame.
 
         self.particles_pub = self.create_publisher(PoseArray, '/posearray', 1)
-        self.frame = '/base_link_pf'
+        
+        self.frame = '/map'
+        # self.frame = '/map'
+        self.curr_time = self.get_clock().now()
 
     def laser_callback(self, scan): 
-        probabilities = self.sensor_model.evaluate(self.particles, scan.intensities)
+        # return
+        self.mutex.acquire(blocking=True)
+        probabilities = self.sensor_model.evaluate(self.particles, scan.ranges)
         try: 
             probabilities /= sum(probabilities)
         except: 
-            probabilities = np.empty(100)
-            probabilities.fill(1/100) 
+            probabilities = np.empty(200)
+            probabilities.fill(1/200)
+        self.get_logger().info(f"probs: {probabilities}")
         resampled_indices = np.random.choice(self.particles.shape[0], size=self.particles.shape[0], replace=True, p=probabilities)
         resampled_particles = self.particles[resampled_indices]
         self.particles = resampled_particles
 
         self.publish_transform(self.particles)
+        self.mutex.release()
 
 
     def odom_callback(self, odometry): 
-        dx = odometry.twist.twist.linear.x
-        dy = odometry.twist.twist.linear.y
-        dth = odometry.twist.twist.angular.z
+        self.mutex.acquire(blocking=True)
+        dt = (self.get_clock().now() - self.curr_time).nanoseconds * 1e-9
+        self.curr_time = self.get_clock().now()
+
+        dx = odometry.twist.twist.linear.x * dt
+        dy = odometry.twist.twist.linear.y * dt
+        dth = odometry.twist.twist.angular.z * dt
         od = [dx, dy, dth]
-        updated_particles = self.motion_model.evaluate(self.particles, od)
+        self.particles = self.motion_model.evaluate(self.particles, od)
         
         # return updated_particles
-        self.particles = updated_particles
         self.publish_transform(self.particles)
-    
-    # def pose_callback(self, msg): 
-    #     """
-    #         var:
-    #             msg = PoseWithCovariance Message, vars: pose, covariance
-    #                 pose = Pose Message, vars: Point position, Quaternion orientation
-    #     """
-    #     #init particles
-    #     # np.random.choice: (original array size, desired sample size, probabilities list of original array)
-    #     # self.particles = np.random.choice(self.particles, )
-    #     x = msg.pose.pose.position.x
-    #     y = msg.pose.pose.position.y
-    #     z = msg.pose.pose.position.z
-
-    #     q_x = msg.pose.pose.orientation.x
-    #     q_y = msg.pose.pose.orientation.y
-    #     q_z = msg.pose.pose.orientation.z
-    #     q_w = msg.pose.pose.orientation.w
-
-
-    #     self.particles = np.array(pose) + np.random.normal(loc=0.0, scale = .001, size=(len(self.particles),3))
-    #     self.get_logger().info("init particles from pose")
+        self.mutex.release()
         
     def pose_callback(self, msg): 
         """
@@ -140,38 +129,36 @@ class ParticleFilter(Node):
             msg = PoseWithCovarianceStamped Message, vars: pose, covariance
                 pose = Pose Message, vars: Point position, Quaternion orientation
         """
+        self.mutex.acquire(blocking=True)
         # Extract position
         x = msg.pose.pose.position.x
         y = msg.pose.pose.position.y
-        th = 2*np.arccos(msg.pose.pose.orientation.w)
-        
-        # self.get_logger().info(str(x))
-        # self.get_logger().info(str(y))
-        # self.get_logger().info(str(th))
+        # euler fr/ q
+        odom_euler = tf.euler_from_quaternion((msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w))
+        # self.get_logger().info(f"odom_quat is: {odom_quat}")
+        th = odom_euler[2] # maybe negative of this?? idk??
 
-        # # Update particles with the new position and orientation
-        # newx = x + np.random.normal(loc=0.0, scale=0.001, size=(len(self.particles), 2))
-        # newy = y +  np.random.normal(loc=0.0, scale=0.001, size=(len(self.particles), 2))
-        # newth = np.angle(np.exp(1j * (th + np.random.default_rng().uniform(low=0.0, high=2*np.pi, size=len(self.particles)))))
-        # # self.get_logger().info(newx, newy, newth)
+        newx = np.array(x + np.random.normal(loc=0.0, scale=0.1, size=(len(self.particles),1)))
+        newy = np.array(y + np.random.normal(loc=0.0, scale=0.1, size=(len(self.particles),1)))
+        newth = np.array(th + np.random.normal(loc=0.0, scale=0.01, size=(len(self.particles),1)))
+        # newx = np.full(shape=(len(self.particles),1), fill_value=x) 
+        # newy = np.full(shape=(len(self.particles),1), fill_value=y)  
+        # newth = np.full(shape=(len(self.particles),1), fill_value=th)  
+         # np.angle(np.exp(1j * (th + np.random.default_rng().uniform(low=0.0, high=2*np.pi, size=(len(self.particles),1)))))
 
-        # # newth = 
-        # # self.particles = np.swapaxes(np.array([newx, newy, newth]))
-        # # self.sensor_model.evaluate(self.particles, )
-        # # self.particles = np.transpose(np.array([newx, newy, newth]))
-        # # self.particles = np.array([np.array([x,y,th]) for x,y,th in zip(newx, newy, newth)])        
-        
-        # self.get_logger().info("Initialized particles from pose")
+        # self.get_logger().info(np.array_str(newx))
+        # self.get_logger().info(np.array_str(newy))
+        # self.get_logger().info(np.array_str(newth))
+        self.particles = np.concatenate((newx, newy, newth), axis=1)
 
-        od = [x, y, th]
-        updated_particles = self.motion_model.evaluate(self.particles, od)
-        
-        # return updated_particles
-        self.particles = updated_particles
+        # self.get_logger().info("*******Initialized particles from pose******")
         self.publish_transform(self.particles)
+        self.mutex.release()
+
 
 
     def publish_transform(self, particles): 
+        # self.mutex.acquire(blocking=True)
         #average particle pose 
         sin_sum = np.sum(np.sin(particles[:,2]))
         cos_sum = np.sum(np.cos(particles[:,2]))
@@ -180,75 +167,34 @@ class ParticleFilter(Node):
         avg_x = np.average(particles[:,0])
         avg_y = np.average(particles[:,1])
         
-        # particle_pose = [avg_x, avg_y, avg_angle]
-        
-        #publish transform between /map frame and frame for exp car base link
-        # tf_map_baselink = self.sensor_model.map @ particle_pose
-        # now = self.get_clock().now()
-        # out = self.se3_to_tf(tf_map_baselink, now, parent='map', child='base_link')
-        
         odom_pub_msg = Odometry() # creating message template in case ros is mad
-        odom_pub_msg.pose.pose.position.x = avg_x #out # specifically ONLY publish to the pose variable
+        odom_pub_msg.pose.pose.position.x = avg_x 
         odom_pub_msg.pose.pose.position.y = avg_y
-        odom_pub_msg.pose.pose.orientation.w = avg_angle
+        odom_pub_msg.pose.pose.position.z = 0.0
+        odom_quat = tf.quaternion_from_euler(0, 0, avg_angle)
+        odom_pub_msg.pose.pose.orientation = Quaternion(x=odom_quat[0], y=odom_quat[1], z=odom_quat[2], w=odom_quat[3])
         #header
 
         odom_pub_msg.header.stamp = rclpy.time.Time().to_msg()
-        odom_pub_msg.header.frame_id = self.frame
+        odom_pub_msg.header.frame_id = "/map"
         self.odom_pub.publish(odom_pub_msg)
 
         #posearray
-        # particles_msg = PoseArray()
-        # particles_msg.header.stamp = rclpy.time.Time().to_msg()
-        # particles_msg.header.frame_id = self.frame
-        # particles_msg.poses = [i[0], i[1], i[2] for i in self.particles]
-        # np.array[]
-        # pose.position
-        # quaternion
-        # self.particles_pub(particles_msg)
-
-
-    def tf_to_se3(self, transform: TransformStamped.transform) -> np.ndarray:
-        """
-        Convert a TransformStamped message to a 4x4 SE3 matrix 
-        """
-        q = transform.rotation
-        q = [q.x, q.y, q.z, q.w]
-        t = transform.translation
-        mat = tf_transformations.quaternion_matrix(q)
-        mat[0, 3] = t.x
-        mat[1, 3] = t.y
-        mat[2, 3] = t.z
-        return mat
-
-    def se3_to_tf(self, mat: np.ndarray, time: Time, parent: str, child: str) -> TransformStamped:
-        """
-        Convert a 4x4 SE3 matrix to a TransformStamped message
-        """
-        obj = geometry_msgs.msg.TransformStamped()
-
-        # current time
-        obj.header.stamp = time.to_msg()
-
-        # frame names
-        obj.header.frame_id = parent
-        obj.child_frame_id = child
-
-        # translation component
-        obj.transform.translation.x = mat[0, 3]
-        obj.transform.translation.y = mat[1, 3]
-        obj.transform.translation.z = mat[2, 3]
-
-        # rotation (quaternion)
-        q = tf_transformations.quaternion_from_matrix(mat)
-        obj.transform.rotation.x = q[0]
-        obj.transform.rotation.y = q[1]
-        obj.transform.rotation.z = q[2]
-        obj.transform.rotation.w = q[3]
-
-        return obj
-
-
+        particles_msg = PoseArray()
+        particles_msg.header.stamp = rclpy.time.Time().to_msg()
+        particles_msg.header.frame_id = "/map"
+        poses = []
+        for x,y,th in self.particles:
+            pose_msg = Pose() 
+            pose_msg.position.x = x
+            pose_msg.position.y = y
+            pose_msg.position.z = 0.0
+            quat = tf.quaternion_from_euler(0, 0, th)
+            pose_msg.orientation = Quaternion(x=quat[0], y=quat[1], z=quat[2], w=quat[3])
+            poses.append(pose_msg)
+        particles_msg.poses = poses
+        self.particles_pub.publish(particles_msg)
+        # self.mutex.release()
 
 def main(args=None):
     rclpy.init(args=args)
